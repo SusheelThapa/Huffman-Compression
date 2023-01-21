b0a2c578d82c38bbecd914dc1dd1c4c69fe7cc47
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-21 16:15:04 +0545

    Update main.cpp with new initialzer file

--- main.cpp
+++ main.cpp
@@ -1,42 +1,18 @@
-#include "headers/header.hpp"
+#include "window.hpp"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	//Starts up SDL and create window
-	if (!init()) {
-		printf("Failed to initialize!");
-	}
-	else
+
+	Window window("Huffman Visualizer");
+
+	while (!window.isWindowClosed())
 	{
+		window.handleEvent();
 
-        //Sample Code to see if everything works
-        SDL_RenderClear(gRenderer);
-        SDL_Rect fillRect = {10, 10, 100, 100};
-        SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 255);
-        SDL_RenderFillRect(gRenderer, &fillRect);
-        SDL_RenderPresent(gRenderer);
+		window.clear({24, 100, 200, 150});
 
-		//Quit when the user wants
-		SDL_Event e;
-		
-		bool quit = false;
-		while (!quit)
-		{
-			while (SDL_PollEvent(&e))
-			{
-				if (e.type == SDL_QUIT) {
-					quit = true;
-				}
-
-				if (e.key.keysym.sym == SDL_SCANCODE_ESCAPE) {
-					quit = true;
-				}
-			}
-		}
+		window.present();
 	}
-
-	//Free resources and close SDL
-	close();
-
+	
 	return 0;
 }

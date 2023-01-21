417237262fb9057145ff85f3e658fefea385c010
Suprim Devkota <suprimdevkota@gmail.com>
2023-01-21 15:16:01 +0545

    Removed bug

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 
 //Using SDL and standard IO
-#include <SDL2/SDL.h>>
+#include <SDL2/SDL.h>
 #include <stdio.h>
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 1000;
-const int SCREEN_HEIGHT = 1000;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 int main( int argc, char* args[] )
 {

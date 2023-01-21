7a401c625e4ef937320efad9a9e6ca93ea113a4b
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-21 15:22:17 +0545

    [Linux]: Script to build the project in Linux

---
+++ build.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+# Useful variables
+file_to_compile="main.cpp"
+output_filename="main"
+compiler_flags="-w"
+SDL_library="-lSDL2"
+
+# print the command to compile files with SDL
+echo "g++ $file_to_compile $includes $compiler_flags $SDL_library -o $output_filename"
+
+# Compiling command
+g++ $file_to_compile $includes $compiler_flags $SDL_library -o $output_filename
+

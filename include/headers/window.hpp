fcd945cc074046484fded60e84eb3fd33ea1cd44
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-21 16:13:21 +0545

    Change variable from global to local
    initialzer file has used global variable which we have changed into
    local variable by encapsulating into classes

---
+++ include/headers/window.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+#include <iostream>
+#include <string>
+
+class Window
+{
+
+private:
+    std::string title;
+
+    static const int width = 400;
+
+    static const int height = 400;
+
+    bool closed = false;
+
+    SDL_Window *window = nullptr;
+
+    SDL_Event e;
+
+public:
+    SDL_Renderer *renderer = nullptr;
+
+private:
+    bool init();
+
+public:
+    Window();
+    Window(std::string);
+
+    ~Window();
+
+    void handleEvent();
+
+    void clear(SDL_Color color);
+
+    void present();
+
+    bool isWindowClosed();
+
+    static const int getWidth();
+
+    static const int getHeight();
+};

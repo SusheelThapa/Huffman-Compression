6a9bc0c8366443a7cb9a6820b9f678d3516899c3
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-21 16:15:29 +0545

    Added include/ file while compiling

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/bash
 
 # Useful variables
-file_to_compile="main.cpp"
+file_to_compile="main.cpp src/*.cpp"
+includes="-I include/headers"
 output_filename="main"
 compiler_flags="-w"
 SDL_library="-lSDL2"

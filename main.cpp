edd98276d81aa83653a057f7e75fe66674abdb51
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-21 17:14:23 +0545

    Render image in background

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "window.hpp"
+#include "texture.hpp"
 
 int main(int argc, char *argv[])
 {
 
 	Window window("Huffman Visualizer");
+	Texture windowBackground;
+
+	std::string background = "resources/background.png";
+	windowBackground.loadFromFile(window, background);
 
 	while (!window.isWindowClosed())
 	{
@@ -11,8 +16,9 @@ int main(int argc, char *argv[])
 
 		window.clear({24, 100, 200, 150});
 
+		windowBackground.render(window, 0, 0);
 		window.present();
 	}
-	
+
 	return 0;
 }
